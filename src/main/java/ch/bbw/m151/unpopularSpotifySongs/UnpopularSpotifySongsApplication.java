da4883bb3419ec073d6f0e33eed5126371cb9947
Henry Joerg <henry.joerg@gmail.com>
2022-09-05 16:45:57 +0200

    initial commit with generated spring boot project

---
+++ src/main/java/ch/bbw/m151/unpopularSpotifySongs/UnpopularSpotifySongsApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.m151.unpopularSpotifySongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnpopularSpotifySongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnpopularSpotifySongsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/m151/unpopularSpotifySongs/UnpopularSpotifySongsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.m151.unpopularSpotifySongs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnpopularSpotifySongsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
