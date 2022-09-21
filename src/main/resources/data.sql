3a5b1913b9481f3dd919d93c57fdd5624dbfca88
Henry Joerg <henry.joerg@gmail.com>
2022-09-21 16:51:28 +0200

    raw data cleaned up and fk added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,6 @@ INSERT INTO ARTIST_GENRE_ENTITY (ARTIST_NAME, GENRE)
 SELECT DISTINCT artist_name, genre
 FROM CSVREAD('src/main/resources/z_genre_of_artists.csv');
 
-INSERT INTO SONG_ENTITY (TRACK_ID, DANCEABILITY, ENERGY, SPEECHINESS, ACOUSTICNESS, INSTRUMENTALNESS, TEMPO, TRACK_NAME)
-SELECT track_id, danceability, energy, speechiness, acousticness, instrumentalness, tempo, track_name
-FROM CSVREAD('src/main/resources/unpopular_songs.csv');
+INSERT INTO SONG_ENTITY (TRACK_ID, DANCEABILITY, ENERGY, SPEECHINESS, ACOUSTICNESS, INSTRUMENTALNESS, TEMPO, TRACK_NAME, TRACK_ARTIST_ARTIST_NAME)
+SELECT track_id, danceability, energy, speechiness, acousticness, instrumentalness, tempo, track_name, track_artist
+FROM CSVREAD('src/main/resources/songs.csv');
